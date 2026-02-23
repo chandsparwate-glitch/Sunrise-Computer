d3d8c499107141c8f2a4331d00f10e38d0ef32e4
Chandrashekhar Parwate <chandsparwate@gmail.com>
2026-02-23 14:30:50 +0530

    facebook domain verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,12 @@ import GoogleReviews from "@/components/GoogleReviews";
 export const metadata = {
   title: "Sunrise Computer Education",
   description: "MKCL Authorized Learning Center - Sangadi",
+  
+  verification: {
+    other: {
+      "facebook-domain-verification": "ax9rpen4ikb8s1375o1ql867wsvjcv",
+    },
+  },
 };
 
 export const viewport = {
